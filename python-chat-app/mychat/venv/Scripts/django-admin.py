9a461f15b649ddf8f40226eafcd63057fb6e69ae
Preston Harms <prestonharms5@gmail.com>
2019-06-13 15:02:13 -0700

    6-13-19: Finally started and completed python chat app tutorial

---
+++ python-chat-app/mychat/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\panda\Git2.0\tutorials\python-chat-app\mychat\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
